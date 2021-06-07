4f23349bc36b847d3e7631694d93363f0771f80b
Zachary Kayyali <zkayyali@redhat.com>
2021-06-07 09:32:43 -0400

    Ensure dependancies and login to registry

--- common/scripts/pull-request-function-test.sh
+++ common/scripts/pull-request-function-test.sh
@@ -32,6 +32,19 @@ then
   tar xz && sudo mv yq_linux_amd64 /usr/bin/yq >/dev/null
 fi
 
+_OPERATOR_SDK_VERSION=v0.18.2
+
+if ! [ -x "$(command -v operator-sdk)" ]; then
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+            curl -L https://github.com/operator-framework/operator-sdk/releases/download/${_OPERATOR_SDK_VERSION}/operator-sdk-${_OPERATOR_SDK_VERSION}-x86_64-linux-gnu -o operator-sdk
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+            curl -L https://github.com/operator-framework/operator-sdk/releases/download/${_OPERATOR_SDK_VERSION}/operator-sdk-${_OPERATOR_SDK_VERSION}-x86_64-apple-darwin -o operator-sdk
+    fi
+    chmod +x operator-sdk
+    sudo mv operator-sdk /usr/local/bin/operator-sdk
+fi
+
+docker login -u $QUAY_USER -p $QUAY_TOKEN
 
 oc login --token="${COLLECTIVE_TOKEN}" --server="${COLLECTIVE_SERVER}"  --insecure-skip-tls-verify
 
@@ -55,6 +68,7 @@ cd ../..
 oc login $(jq -r '.api_url' ./lifeguard/clusterclaims/${CLUSTERCLAIM_NAME}/${CLUSTERCLAIM_NAME}.creds.json) -u kubeadmin -p $(jq -r '.password' ./lifeguard/clusterclaims/${CLUSTERCLAIM_NAME}/${CLUSTERCLAIM_NAME}.creds.json) --insecure-skip-tls-verify=true
 oc project
 
+docker login quay.io/rhibmcollab -u $QUAY_USER -p $QUAY_TOKEN
 make prep-mock-install MOCK_IMAGE_REGISTRY='quay.io/rhibmcollab' MOCK_IMAGE_NAME='multiclusterhub-operator' MOCK_IMAGE_TAG='mock'
 make mock-install MOCK_IMAGE_REGISTRY='quay.io/rhibmcollab' MOCK_IMAGE_NAME='multiclusterhub-operator' MOCK_IMAGE_TAG='mock'
 make ft-install MOCK=true
